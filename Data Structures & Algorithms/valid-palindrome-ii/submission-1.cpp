7a8362db581f4b518a5c593fbf39f11cd9980a00
Pancham <pancham.mehta.13728018@gmail.com>
2026-06-23 16:29:35 +0530

    Add: valid-palindrome-ii - submission-1

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    bool isPalindrome(string s, int i , int j){
+        while(i<j){
+            if(s[i]==s[j]){
+                i++;
+                j--;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+public:
+    bool validPalindrome(string s) {
+        int i = 0;
+        int j = s.size()-1;
+        while(i<j){
+            if(s[i]!=s[j]){
+                return isPalindrome(s,i+1, j)||isPalindrome(s, i, j-1);
+            }
+            i++;j--;
+        }
+        return true;
+    }
+};
